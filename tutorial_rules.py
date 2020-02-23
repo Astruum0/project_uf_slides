e55aa735306c5d0b4fa57c9f71d945862b6877bf
arthur.vella@ynov.com <arthur.vella@ynov.com>
2020-02-23 18:28:11 +0100

    Bug fixes

--- tutorial_rules.py
+++ tutorial_rules.py
@@ -3,11 +3,7 @@ from moviepy.editor import VideoFileClip
 
 
 def tutorial():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
-        tutorial = VideoFileClip("sound_video/movie_tutorial.mp4")
-        tutorial.preview(fps=60)
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                main_menu()
+    tutorial = VideoFileClip("sound_video/movie_tutorial.mp4")
+    tutorial.preview(fps=60)
+    return
+
